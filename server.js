d27aa7e8ffc31523f3333c671dd709b4384b1bf9
bcrraKevinho <becerra.kevin@uabc.edu.mx>
2023-06-06 11:41:36 -0700

    change of port

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world with express!'));
+app.get('/', (req, res) => {
+    res.sendFile('/index.html, { root: __dirname }');
+});
 
 app.listen(3000);
 console.log('Server running at port 3000');
