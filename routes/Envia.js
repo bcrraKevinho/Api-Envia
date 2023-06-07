84f573014ae0437a7858b7aca8e84646461fe382
bcrraKevinho <becerra.kevin@uabc.edu.mx>
2023-06-07 12:51:17 -0700

    Changed the root page

--- routes/Envia.js
+++ routes/Envia.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const EnviaCtrl = require("../controllers/EnviaController");
 
-router.get('/tracking', EnviaCtrl.tracking);
+router.get('/', EnviaCtrl.tracking);
 router.get('/create', EnviaCtrl.create);
 router.get('/update', EnviaCtrl.update);
 router.get('/remove', EnviaCtrl.remove);
--- server.js
+++ server.js
@@ -6,11 +6,6 @@ app.set("view engine", "ejs");
 const userRoute = require('./routes/Envia');
 app.use('/', userRoute);
 
-app.get('/', function(req, res) { 
-  res.render('index', {
-  });
-});
-
 app.use(express.urlencoded({ extended: false }));
  
 
