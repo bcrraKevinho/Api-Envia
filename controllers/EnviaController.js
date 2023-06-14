e919b31b8d4d5cac9e941c42e8d1daf9efaaad77
bcrraKevinho <becerra.kevin@uabc.edu.mx>
2023-06-14 13:23:18 -0700

    Create label implemented

--- controllers/EnviaController.js
+++ controllers/EnviaController.js
@@ -28,8 +28,7 @@ module.exports = {
     },
 
     create: (req, res) => {
-        
-        
+                
         if(req.method == 'GET'){
             switch(req.query.submit){
                 case "update":
@@ -44,7 +43,6 @@ module.exports = {
                     };
                     request(options, function (error, response) {
                     if (error) throw new Error(error);
-                        console.log(response.body);
                         res.render('create', {page_name: "update", color: "primary", req: response.body.data});
                     });
                     break;
@@ -57,92 +55,22 @@ module.exports = {
             }
             
         }
-/*
-        if(req.method == 'POST'){
-            var request = require('request');
-            var options = {
-                'method': 'POST',
-                'url': 'https://api-test.envia.com/ship/generate/',
-                'headers': {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
-                },
-                body: JSON.stringify({
-                    "origin": {
-                        "name": req.body.nombreOrigen,
-                        "company": req.body.empresaOrigen,
-                        "email": req.body.emailOrigen,
-                        "phone": req.body.telefonoOrigen,
-                        "street": req.body.calleOrigen,
-                        "number": req.body.numeroOrigen,
-                        "district": req.body.coloniaOrigen,
-                        "city": req.body.ciudadOrigen,
-                        "state": req.body.estadoOrigen,
-                        "country": req.body.paisOrigen,
-                        "postalCode": req.body.cpOrigen,
-                        "reference": req.body.referenciaOrigen
-                    },
-                    "destination": {
-                        "name": req.body.nombreDestino,
-                        "company": req.body.empresaDestino,
-                        "email": req.body.emailDestino,
-                        "phone": req.body.telefonoDestino,
-                        "street": req.body.calleDestino,
-                        "number": req.body.numeroDestino,
-                        "district": req.body.coloniaDestino,
-                        "city": req.body.ciudadDestino,
-                        "state": req.body.estadoDestino,
-                        "country": req.body.paisDestino,
-                        "postalCode": req.body.cpDestino,
-                        "reference": req.body.referenciaDestino
-                    },
-                    "packages": [
-                        {
-                        "content": req.body.contenido,
-                        "amount": Number(req.body.cantidad),
-                        "type": req.body.tipo,
-                        "dimensions": {
-                            "length": Number(req.body.largo),
-                            "width": Number(req.body.ancho),
-                            "height": Number(req.body.alto)
-                        },
-                        "weight": Number(req.body.peso),
-                        "insurance": Number(req.body.seguro),
-                        "declaredValue": Number(req.body.valor),
-                        "weightUnit": "KG",
-                        "lengthUnit": "CM"
-                        }
-                    ],
-                    "shipment": {
-                        "carrier": "fedex",
-                        "service": "express",
-                        "type": 1
-                    },
-                    "settings": {
-                        "printFormat": "PDF",
-                        "printSize": "STOCK_4X6",
-                        "comments": "comentarios de el envío"
-                    }
-                })
-            };
-            
-            request(options, function (error, response) {
-                if (error) throw new Error(error);
-                console.log(response.body);
 
-                res.render('create', 
-                {page_name: "create", color: "success", 
-                req: req.body, res: response.data, 
-                disabled: "disabled", modal: "<script>$(window).on('load', function() {$('#exampleModal').modal('show');});</script>"});
-              });
-        }
-*/
         if(req.method == 'POST'){    
-            var request = require('request');
-
-            async function getQuote(carrier){
-                return new Promise((resolve, reject) => {
-                    var bodyJson = {
+            var request = require('request'); 
+            //Generar guía
+            if(typeof req.body.btnGenerar !== "undefined"){ 
+                var carrier = req.body.btnGenerar.split(",")[0];
+                var service = req.body.btnGenerar.split(",")[1];
+                
+                var options = {
+                    'method': 'POST',
+                    'url': 'https://api-test.envia.com/ship/generate/',
+                    'headers': {
+                      'Content-Type': 'application/json',
+                      'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
+                    },
+                    body: JSON.stringify({
                         "origin": {
                             "name": req.body.nombreOrigen,
                             "company": req.body.empresaOrigen,
@@ -172,93 +100,166 @@ module.exports = {
                             "reference": req.body.referenciaDestino
                         },
                         "packages": [
-                        {
+                            {
                             "content": req.body.contenido,
                             "amount": Number(req.body.cantidad),
                             "type": req.body.tipo,
-                            "weight": Number(req.body.peso),
-                            "insurance": Number(req.body.seguro),
-                            "declaredValue": Number(req.body.valor),
-                            "weightUnit": "KG",
-                            "lengthUnit": "CM",
                             "dimensions": {
                                 "length": Number(req.body.largo),
                                 "width": Number(req.body.ancho),
                                 "height": Number(req.body.alto)
+                            },
+                            "weight": Number(req.body.peso),
+                            "insurance": Number(req.body.seguro),
+                            "declaredValue": Number(req.body.valor),
+                            "weightUnit": "KG",
+                            "lengthUnit": "CM"
                             }
-                        }
                         ],
                         "shipment": {
-                        "carrier": carrier,
-                        "type": 1
+                            "carrier": carrier,
+                            "service": service,
+                            "type": 1
                         },
                         "settings": {
-                        "printFormat": "PDF",
-                        "printSize": "STOCK_4X6",
-                        "currency": "MXN",
-                        "cashOnDelivery": "500.00",
-                        "comments": ""
+                            "printFormat": "PDF",
+                            "printSize": "STOCK_4X6",
+                            "comments": "comentarios de el envío"
                         }
-                    };
-                    var options = {
-                        'method': 'POST',
-                        'url': 'https://api-test.envia.com/ship/rate/',
-                        'headers': {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
-                        },
-                        body: JSON.stringify(bodyJson)    
-                    };
-                    request(options, function (error, response) {
-                        var quotes = [];
-                        if (error) throw new Error(error);
-                            json2 = JSON.parse(response.body);
-                        
-                            if(json2.meta === "rate" && typeof json2.data === "object"){
-                                for(var j = 0; j < json2.data.length; j++)
-                                {
-                                    if(json2.data[j].carrier !== "")
-                                        quotes.push({
-                                            carrier: json2.data[j].carrier,
-                                            service: json2.data[j].service,
-                                            deliveryEstimate: json2.data[j].deliveryEstimate,
-                                            totalPrice: json2.data[j].totalPrice
-                                        });
+                    })
+                };
+                
+                request(options, function (error, response) {
+                    if (error) throw new Error(error);
+                    res.render('create', 
+                    {page_name: "create", color: "success", 
+                    req: req.body, res: JSON.parse(response.body).data, 
+                    disabled: "disabled", modal_title: "Guía generada con éxito",
+                    modal: "<script>$(window).on('load', function() {$('#exampleModal').modal('show');});</script>"});
+                  });
+                return;
+            }
+            //Cotizar
+            else {
+                async function getQuote(carrier){ //Cotiza con una empresa
+                    return new Promise((resolve, reject) => {
+                        var bodyJson = {
+                            "origin": {
+                                "name": req.body.nombreOrigen,
+                                "company": req.body.empresaOrigen,
+                                "email": req.body.emailOrigen,
+                                "phone": req.body.telefonoOrigen,
+                                "street": req.body.calleOrigen,
+                                "number": req.body.numeroOrigen,
+                                "district": req.body.coloniaOrigen,
+                                "city": req.body.ciudadOrigen,
+                                "state": req.body.estadoOrigen,
+                                "country": req.body.paisOrigen,
+                                "postalCode": req.body.cpOrigen,
+                                "reference": req.body.referenciaOrigen
+                            },
+                            "destination": {
+                                "name": req.body.nombreDestino,
+                                "company": req.body.empresaDestino,
+                                "email": req.body.emailDestino,
+                                "phone": req.body.telefonoDestino,
+                                "street": req.body.calleDestino,
+                                "number": req.body.numeroDestino,
+                                "district": req.body.coloniaDestino,
+                                "city": req.body.ciudadDestino,
+                                "state": req.body.estadoDestino,
+                                "country": req.body.paisDestino,
+                                "postalCode": req.body.cpDestino,
+                                "reference": req.body.referenciaDestino
+                            },
+                            "packages": [
+                            {
+                                "content": req.body.contenido,
+                                "amount": Number(req.body.cantidad),
+                                "type": req.body.tipo,
+                                "weight": Number(req.body.peso),
+                                "insurance": Number(req.body.seguro),
+                                "declaredValue": Number(req.body.valor),
+                                "weightUnit": "KG",
+                                "lengthUnit": "CM",
+                                "dimensions": {
+                                    "length": Number(req.body.largo),
+                                    "width": Number(req.body.ancho),
+                                    "height": Number(req.body.alto)
                                 }
-                                resolve(quotes);
                             }
-                            resolve([{carrier: "No disponible", service: "No disponible", deliveryEstimate: "No disponible", totalPrice: "No disponible"}]);
-                        }
-                        );
-                });}
+                            ],
+                            "shipment": {
+                            "carrier": carrier,
+                            "type": 1
+                            },
+                            "settings": {
+                            "printFormat": "PDF",
+                            "printSize": "STOCK_4X6",
+                            "currency": "MXN",
+                            "cashOnDelivery": "500.00",
+                            "comments": ""
+                            }
+                        };
+                        var options = {
+                            'method': 'POST',
+                            'url': 'https://api-test.envia.com/ship/rate/',
+                            'headers': {
+                                'Content-Type': 'application/json',
+                                'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
+                            },
+                            body: JSON.stringify(bodyJson)    
+                        };
+                        request(options, function (error, response) {
+                            var quotes = [];
+                            if (error) throw new Error(error);
+                                json2 = JSON.parse(response.body);
+                            
+                                if(json2.meta === "rate" && typeof json2.data === "object"){
+                                    for(var j = 0; j < json2.data.length; j++)
+                                    {
+                                        if(json2.data[j].carrier !== "")
+                                            quotes.push({
+                                                carrier: json2.data[j].carrier,
+                                                service: json2.data[j].service,
+                                                deliveryEstimate: json2.data[j].deliveryEstimate,
+                                                totalPrice: json2.data[j].totalPrice
+                                            });
+                                    }
+                                    resolve(quotes);
+                                }
+                                resolve([{carrier: "No disponible", service: "No disponible", deliveryEstimate: "No disponible", totalPrice: "No disponible"}]);
+                            }
+                            );
+                    });}
 
-            async function getQuotes(){
-                return new Promise((resolve, reject) => {
-                    var options = {
-                    'method': 'GET',
-                    'url': 'https://queries-test.envia.com/available-carrier/MX/0',
-                    'headers': {
-                        'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
-                    }
-                    };
-                    request(options, async function (error, response) {
-                        var quotes = [];
-                        if (error) throw new Error(error);    
-                        json = JSON.parse(response.body);
-                        for(var i = 0; i < json.data.length; i++){
-                            quotes.push(await getQuote(json.data[i].name));
+                async function getQuotes(){ //Cotiza con todas las empresas
+                    return new Promise((resolve, reject) => {
+                        var options = {
+                        'method': 'GET',
+                        'url': 'https://queries-test.envia.com/available-carrier/MX/0',
+                        'headers': {
+                            'Authorization': 'Bearer 51b7a0483cecd5ab66d9dffdf3c62d4d90128ec6b91a6087328f100ca71bc514'
                         }
-                        resolve(quotes);
-                    });  
-                })};
-            
-            getQuotes().then((quotes) => { console.log(quotes); 
-                res.render('create', 
-                {page_name: "create", color: "success", 
-                req: req.body, res: quotes, 
-                modal: "<script>$(window).on('load', function() {$('#exampleModal').modal('show');});</script>"});
-              });
-            
+                        };
+                        request(options, async function (error, response) {
+                            var quotes = [];
+                            if (error) throw new Error(error);    
+                            json = JSON.parse(response.body);
+                            for(var i = 0; i < json.data.length; i++){
+                                quotes.push(await getQuote(json.data[i].name));
+                            }
+                            resolve(quotes);
+                        });  
+                    })};
+                
+                getQuotes().then((quotes) => { //Renderiza la vista con las cotizaciones
+                    res.render('create', 
+                    {page_name: "create", color: "success", 
+                    req: req.body, quotes: quotes, modal_title: "Cotizaciones para tu envío",
+                    modal: "<script>$(window).on('load', function() {$('#exampleModal').modal('show');});</script>"});
+                });
+            }
         }
     },
 
